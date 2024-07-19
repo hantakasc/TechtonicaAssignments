d485e942a5a9a68611ea288b6ec6247bc47438ec
Christina Hantakas <christinahantakas@Christinas-MacBook-Air.local>
2024-07-19 14:56:47 -0700

    add an ingredient checklist

---
+++ recipe/js/script.js
@@ -0,0 +1,27 @@
+var newListItem = document.createElement('li');
+newListItem.textContent = 'Lots of love';
+document.querySelector('ul').appendChild(newListItem);
+
+const myDiv = document.getElementById("myDiv");
+
+const ingredients = [
+    { id: "walnuts-checkbox", value: "walnuts", text: "Add walnuts" },
+    { id: "cinnamon-checkbox", value: "cinnamon", text: "Add cinnamon" },
+    { id: "nutmeg-checkbox", value: "nutmeg", text: "Add nutmeg" },
+    { id: "love-checkbox", value: "love", text: "Add LOTS of love"}
+];
+
+ingredients.forEach(ingredient => {
+    let checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    checkbox.name = "ingredient";
+    checkbox.value = ingredient.value;
+    checkbox.id = ingredient.id;
+
+    let label = document.createElement("label");
+    label.htmlFor = ingredient.id;
+    label.textContent = ingredient.text;
+
+    myDiv.appendChild(checkbox);
+    myDiv.appendChild(label);
+});
