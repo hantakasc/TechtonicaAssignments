026a27d4ce557b3a01ae165674a122fc0a498924
Christina Hantakas <christinahantakas@Christinas-MacBook-Air.local>
2024-08-06 13:35:38 -0700

    Add statement to create object

--- mini-project/event.js
+++ mini-project/event.js
@@ -1,3 +1,4 @@
+//Step 1
 class Event {
     constructor(name, description) {
         this.name = name;
@@ -5,3 +6,10 @@ class Event {
         this.availableTickets = [];
     }
 }
+
+//Step 2
+//This will create an obect!
+const eventObj1 = new Event(
+    'KLOS Golden Gala', //name
+    'An evening with hollywood vampires' //description
+);
