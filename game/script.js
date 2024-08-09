0aa033c54bb01225a683e45130881c72afb47541
Christina Hantakas <christinahantakas@Christinas-MacBook-Air.local>
2024-08-08 22:01:25 -0700

    Random number generator between 1-100

--- game/script.js
+++ game/script.js
@@ -6,4 +6,11 @@ document.addEventListener('DOMContentLoaded', () => {
     const feedback = document.getElementById('feedback');
     const wrongGuessCount = document.getElementById('wrong-guess-count');
     const guessedNumbers = document.getElementById('guessed-numbers');
+
+  //Generate a random number between 1 and 100 as the secret number
+    let secretNumber = Math.floor(Math.random() * 100) + 1;
+    //Initialize the count of wrong guesses
+    let wrongGuesses = 0;
+    //Set to store guessed numbers and prevent duplicates
+    let guessedNumbersSet = new Set();
 })
