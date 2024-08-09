8598ce183b01a1bd021b89d0a74922c8294bb226
Christina Hantakas <christinahantakas@Christinas-MacBook-Air.local>
2024-08-08 21:56:45 -0700

    Get DOM elements

---
+++ game/script.js
@@ -0,0 +1,9 @@
+//Wait for HTML document to be loaded
+document.addEventListener('DOMContentLoaded', () => {
+    //Get DOM elements by their IDs
+    const guessInput = document.getElementById('guess-input');
+    const submitGuess = document.getElementById('submit-guess');
+    const feedback = document.getElementById('feedback');
+    const wrongGuessCount = document.getElementById('wrong-guess-count');
+    const guessedNumbers = document.getElementById('guessed-numbers');
+})
