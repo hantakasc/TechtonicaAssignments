6b8e855596ab4cd66832c9769ceca42766265df2
Christina Hantakas <christinahantakas@Christinas-MacBook-Air.local>
2024-08-22 20:08:51 -0700

    add initial data

---
+++ week-six-project/songs.js
@@ -0,0 +1,75 @@
+const SONGS = [
+    {
+        "artist": "SiR",
+        "song": "Satisfaction",
+        "album": "HEAVY",
+        "year": "2022"
+    },
+    {
+        "artist": "ScHoolboy Q",
+        "song": "Blueslides",
+        "album": "BLUE LIPS",
+        "year": "2024",
+    },
+    {
+        "artist": "Ravyn Lenae",
+        "song": "Closer (Ode 2 U)",
+        "album": "Crush - EP",
+        "year": "2018",
+    },
+    {
+        "artist": "El General",
+        "song": "Muevelo",
+        "album": "El General",
+        "year": "1998",
+    },
+    {
+        "artist": "Roy Ayers Ubiquity",
+        "song": "Searching",
+        "album": "Vibrations",
+        "year": "1976",
+    },
+    {
+        "artist": "Cleo Soul",
+        "song": "Her Light",
+        "album": "Rose that Grew in the Dark",
+        "year": "2020",
+    },
+    {
+        "artist": "Kaytranada",
+        "song": "Oh No (feat. Estelle)",
+        "album": "BUBBA",
+        "year": "2019",
+    },
+    {
+        "artist": "Chicano Batman",
+        "song": "Black Lipstick",
+        "album": "Black Lipstick - Single",
+        "year": "2015",
+    },
+    {
+        "artist": "Kanye West",
+        "song": "Addiction",
+        "album": "Late Registration",
+        "year": "2005",
+    },
+    {
+        "artist": "Solange",
+        "song": "Crush",
+        "album": "Solo Star",
+        "year": "2002",
+    },
+    {
+        "artist": "Mac DeMarco",
+        "song": "One More Love Song",
+        "album": "This Old Dog",
+        "year": "2017",
+    },
+    {
+        "artist": "Jorge Ben Jor",
+        "song": "Chove Chuva",
+        "album": "Samba Esquema Novo",
+        "year": "1963",
+    }
+]
+export default SONGS;
